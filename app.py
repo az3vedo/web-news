05aaf62ad631d46d91f9149d8771f8ff824940ab
Gabriel Souza <azevedoedit@gmail.com>
2022-03-20 16:53:17 -0300

    add pagina de noticias por categoria

--- app.py
+++ app.py
@@ -5,18 +5,31 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 app = Flask(__name__)
 noticias = [
             {
-              "titulo": "Acidente de carro",
+              "titulo": "Time da cidade passa para as semi-finais!",
               "autor": "Pedro Cabral",
-              "categoria": "sensacionalismo",
+              "categoria": "Esporte",
               "materia": "Pedro Paulo Ricardo Gabriel Maria Fabricio Fernando José Raul William Douglas Charles Beatriz Vitória Bianca"
             },
             {
-              "titulo": "Ganhador da loteria",
+              "titulo": "Vai chover canivetes!",
               "autor": "João das Neves",
-              "categoria": "falta do que fazer",
+              "categoria": "Clima e previsão do tempo",
+              "materia": "Pedro Paulo Ricardo Gabriel Maria Fabricio Fernando José Raul William Douglas Charles Beatriz Vitória Bianca"
+            },
+            {
+              "titulo": "Novo esporte domina as escolas da cidade",
+              "autor": "João das Neves",
+              "categoria": "Esporte",
               "materia": "Pedro Paulo Ricardo Gabriel Maria Fabricio Fernando José Raul William Douglas Charles Beatriz Vitória Bianca"
             }
           ]
+categorias = "EsportevwsOportunidade e empregovwsClima e previsão do tempovwsCiência e Tecnologia".split('vws')
+
+def select_article_category(category):
+  result = list(filter(lambda x: x["categoria"] == category, noticias))
+  return result
+
+
 env = Environment(
   loader=PackageLoader("app"),
   autoescape=select_autoescape()
@@ -30,5 +43,9 @@ def home():
 @app.route("/categorias")
 def category():
   template = env.get_template("categorias.html")
-  return render_template(template, page={"title":"Categorias"})
+  return render_template(template,categorias=categorias, page={"title":"Categorias"})
 
+@app.route("/categorias/<categoria>")
+def show_category(categoria):
+  template = env.get_template("index.html")
+  return render_template(template,noticias=select_article_category(categoria), page={"title":"Notícias"})
