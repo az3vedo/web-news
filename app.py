e472c68162fcfad1726635672468cb87388428cb
Gabriel Souza <azevedoedit@gmail.com>
2022-04-01 08:29:15 -0300

    Altera as configurações de execução para o app.py e remove as variáveis de ambiente do setup.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from jinja2 import Environment, PackageLoader, select_autoescape
 
+# Informações de execução, não alterar
 
+PORT=5001
 app = Flask(__name__)
+if(__name__=='__main__'):
+  app.run('0.0.0.0', port=PORT)
+
+#######################################
 noticias = [
             {
               "titulo": "Time da cidade passa para as semi-finais!",
--- setup.py
+++ setup.py
@@ -6,7 +6,5 @@ try:
 except:
     env = "production"
 
-os.environ['FLASK_APP']="app"
-os.environ['FLASK_ENV']=env
 os.system('pip install -r ./requirements.txt')
-os.system('flask run --port 5001')
+os.system('flask run')
